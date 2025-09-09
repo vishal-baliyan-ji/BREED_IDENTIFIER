6ca4a5178add0d2c079eff4ca83cc604959b3d4f
Vishal Baliyan <vishalbaliyan502@gmail.com>
2025-09-09 17:17:08 +0530

    Added layout for login page

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ def home():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+@app.route("/login")
+def login():
+    return render_template('login.html')
 if __name__ == '__main__':
    app.run(debug=True)

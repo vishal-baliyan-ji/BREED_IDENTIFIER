0b55bb0501f1db2c2ada5d1f38f87399628c6124
Vishal Baliyan <vishalbaliyan502@gmail.com>
2025-09-09 22:56:51 +0530

    Login frontend updated and layout improved

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,request
 import jinja2
 app = Flask(__name__)
 
@@ -11,5 +11,11 @@ def contact():
 @app.route("/login")
 def login():
     return render_template('login.html')
+@app.route('/submit', methods=['POST'])
+def submit():
+    username = request.form['usrid']
+    password = request.form['password']
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
    app.run(debug=True)

ef34941d34c179ebbc1fefbb063181e536d6e8e8
Vishal Baliyan <vishalbaliyan502@gmail.com>
2025-09-11 03:20:24 +0530

    added login feature backend and added file uploader to identify

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
-from flask import Flask,render_template,redirect,url_for,request
+from flask import Flask,render_template,redirect,url_for,request,session,flash
 import jinja2
-app = Flask(__name__)
 
+
+import mysql.connector
+mydb =mysql.connector.connect(host="localhost",user="root",passwd="Roomno775",database="pashudhan")
+
+cursor=mydb.cursor()
+
+
+app = Flask(__name__)
+app.secret_key="raja-moli"
 @app.route("/")
 def home():
-    return render_template('index.html')
+    if('user' in session):
+        return render_template('index.html',status='Logout')
+    else:
+        return render_template('index.html',status='Login')
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session.clear() 
+    return redirect(url_for("home"))
+
+
 @app.route('/submit', methods=['POST'])
 def submit():
     username = request.form['usrid']
     password = request.form['password']
-    return redirect(url_for('home'))
-
+    sql="select password, name from drs_data where id=%s"
+    cursor.execute(sql,(username,))
+    data=cursor.fetchone()
+    pwd=data[0]
+    name=data[1]
+    if (password==pwd):
+        session['user']=username
+        session['name']=name
+        return redirect(url_for('home'))
+    else:
+        return "Wrong password"
+    
 @app.route("/register")
 def register():
-    return render_template('register.html')
+    if 'user' in session:
+        return render_template('register.html')
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/registeranimal',methods=['POST'])
 def registeranimal():
@@ -38,7 +69,10 @@ def identifyanimal():
 
 @app.route("/search")
 def search():
-    return render_template('search.html')
+    if 'user' in session:
+        return render_template('search.html')
+    else:
+        return redirect(url_for('login'))
 
 if __name__ == '__main__':
    app.run(debug=True)
